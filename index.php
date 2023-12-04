69c54d0d34403dada56c925806461cd3a3b0d7c4
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-12-05 00:15:09 +0100

    Changed overall desing of the dashboard page

--- index.php
+++ index.php
@@ -15,7 +15,31 @@ if (!isset($_SESSION['user_id'])) {
     <link rel="stylesheet" href="frontend/css/dashboard.css">
 </head>
 <body>
-
+    <div id="menu">
+        <div id="menu-left">
+            <div id="logo">
+                <img src="frontend/assets/icons/icon.png" alt="logo">
+            </div>
+            <div id="menu-links">
+                <a href="index.php">Strona główna</a>
+                <a href="frontend/html/projects.php">Wszystkie projekty</a>
+                <a href="frontend/html/my_projects.php">Moje projekty</a>
+            </div>
+        </div>
+        <div id="menu-right">
+            <div id="user">
+                <div id="user-name">
+                    <?php echo $_SESSION['username']; ?>
+                </div>
+                <div id="user-avatar">
+                    <img src="frontend/img/avatar.png" alt="avatar">
+                </div>
+            </div>
+            <div id="menu-links">
+                <a href="backend/php/logout.php">Wyloguj</a>
+            </div>
+        </div>
+        </div>
     <div id="all-projects">
         
     </div>
@@ -23,7 +47,9 @@ if (!isset($_SESSION['user_id'])) {
     <div id="my-projects">
         
     </div>
-
+    <script>
+window.userRoleId = <?php echo json_encode($_SESSION['role_id']); ?>;
+</script>
     <script src="frontend/js/dashboard.js"></script>
 </body>
 </html>
